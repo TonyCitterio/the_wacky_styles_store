af8cff73d2a5d3dbec0f3f770ed0ce0ad283ff7b
TonyCitterio <tcitterio@hotmail.com>
2023-11-01 16:40:03 +0100

    done so far

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import classes from "./MainPage.module.css"
+import Navbar from "./general/Navbar";
+import Banner from "./content/Banner";
 
 const MainPage = () => {
   return (
     <>
-      <h1>Tjabba hej</h1>
+    <Navbar />
+    <main className={classes.main}>
+      <Banner />
+    </main>
     </>
   );
 };
---
+++ src/components/content/Banner.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import classes from "./Banner.module.css";
+
+const Banner = () => {
+  return (
+   <section className={classes.container}>
+    <div className={classes.content}>
+      <div></div>
+      <div></div>
+    </div>
+   </section>
+  );
+};
+
+export default Banner;
