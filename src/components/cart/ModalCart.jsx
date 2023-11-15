93492bfa684b1d066c4aa29117533c99a546d84f
TonyCitterio <tcitterio@hotmail.com>
2023-11-15 23:44:00 +0100

    done with topProduct so far

--- src/components/cart/ModalCart.jsx
+++ src/components/cart/ModalCart.jsx
@@ -71,3 +71,142 @@ const ModalCart = ({
 };
 
 export default ModalCart;
+
+// If I might use a portal instead. If I do dont forget to add this <div id="modal-root"></div> in index.
+/* import React from "react";
+import ReactDOM from "react-dom";
+import classes from "./ModalCart.module.css";
+
+const ModalOverlay = ({
+  setModalOpenCart,
+  setCart,
+  removeProductFromCart,
+  addProductToCart,
+  groupedCart,
+  cartIsEmpty,
+  emptyCart,
+  calculateTotalPrice,
+}) => {
+  return (
+    <div className={classes.backdrop}>
+      <div className={classes.cart}>
+        <div className={classes.header}>
+          <h3>Varukorg</h3>
+          <button onClick={() => setModalOpenCart(false)}>Close</button>
+        </div>
+        {cartIsEmpty ? <p>Din varukorg är tom</p> : null}
+        <div className={classes.products}>
+          {groupedCart.map((product) => (
+            <div className={classes.card} key={product.id}>
+              <img
+                src={product.picture}
+                alt={` off coffee for a fake coffee shop`}
+                height={90}
+                width={65}
+              />
+              <p>{product.name}</p>
+              <p>{product.quantity}</p>
+              <p>Price per st: {product.price} kr</p>
+              <p>Summa {calculateTotalPrice(product)} kr</p>
+              <div>
+                <button onClick={() => addProductToCart(product)}>+</button>
+                <button onClick={() => removeProductFromCart(product)}>
+                  -
+                </button>
+              </div>
+            </div>
+          ))}
+          {emptyCart ? (
+            <button onClick={() => setCart([])}>Tom varukorg</button>
+          ) : null}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const ModalCart = ({
+  setModalOpenCart,
+  cart,
+  setCart,
+  removeProductFromCart,
+  addProductToCart,
+}) => {
+  const groupProductsByProductId = (cartProducts) => {
+    const groupedProducts = {};
+
+    cartProducts.forEach((product) => {
+      if (groupedProducts[product.id]) {
+        groupedProducts[product.id].quantity += 1;
+      } else {
+        groupedProducts[product.id] = { ...product, quantity: 1 };
+      }
+    });
+
+    return Object.values(groupedProducts);
+  };
+
+  const groupedCart = groupProductsByProductId(cart);
+  const cartIsEmpty = cart.length === 0;
+  const emptyCart = cart.length >= 1;
+
+  const calculateTotalPrice = (product) => {
+    const price = parseFloat(product.price.replace(/[^\d.-]/g, ""));
+    const totalPrice = product.quantity * price;
+
+    return totalPrice;
+  };
+
+   return (
+  <div className={classes.backdrop}>
+      <div className={classes.cart}>
+        <div className={classes.header}>
+          <h3>Varukorg</h3>
+          <button onClick={() => setModalOpenCart(false)}>Close</button>
+        </div>
+        {cartIsEmpty ? <p>Din varukorg är tom</p> : null}
+        <div className={classes.products}>
+          {groupedCart.map((product) => (
+            <div className={classes.card} key={product.id}>
+              <img
+                src={product.picture}
+                alt={` off coffee for a fake coffee shop`}
+                height={90}
+                width={65}
+              />
+              <p>{product.name}</p>
+              <p>{product.quantity}</p>
+              <p>Price per st: {product.price} kr</p>
+              <p>Summa {calculateTotalPrice(product)} kr</p>
+              <div>
+                <button onClick={() => addProductToCart(product)}>+</button>
+                <button onClick={() => removeProductFromCart(product)}>-</button>
+              </div>
+            </div>
+          ))}
+          {emptyCart ? (
+            <button onClick={() => setCart([])}>Tom varukorg</button>
+          ) : null}
+        </div>
+      </div>
+    </div> 
+  );
+  return (
+    <>
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          setModalOpenCart={setModalOpenCart}
+          setCart={setCart}
+          removeProductFromCart={removeProductFromCart}
+          addProductToCart={addProductToCart}
+          groupedCart={groupedCart}
+          cartIsEmpty={cartIsEmpty}
+          emptyCart={emptyCart}
+          calculateTotalPrice={calculateTotalPrice}
+        />, document.getElementById('modal-root')
+      )}
+    </>
+  );
+};
+
+export default ModalCart; */
--- src/components/content/Products.jsx
+++ src/components/content/Products.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 import classes from "./Products.module.css";
 
-const Products = ({ products }) => {
-  
-  const getProduct = (product) => {
-    alert(`Produkt: ${product.name} Pris: ${product.price} ${product.picture}`);
-  };
-console.log(products)
+const Products = ({ products, addProductToCart, removeProductFromCart }) => {
+  const topCoffee = products.slice(1, 5);
+  const recommendation = products.slice(7, 9);
+
   return (
     <div className={classes.container}>
       <div className={classes.content}>
+        <h2>Månadens bästsäljare</h2>
         <div className={classes.products}>
-          {products.map((product) => (
+          {topCoffee.map((product) => (
             <div key={product.id} className={classes.card}>
               <img
                 src={product.picture}
@@ -23,9 +22,41 @@ console.log(products)
               <p>{product.price}</p>
               <p>{product.description}</p>
               <p>{product.roast}</p>
-              <button onClick={() => getProduct(product)}>Get me</button>
+              <button onClick={() => addProductToCart(product)}>Get me</button>
+              <button onClick={() => removeProductFromCart(product)}>
+                Ta bort
+              </button>
             </div>
           ))}
+        </div>
+        <div className={classes.recommendations}>
+          <div>
+            <h2>Tjabba</h2>
+          </div>
+          <div>
+            <div className={classes.products}>
+              {recommendation.map((product) => (
+                <div key={product.id} className={classes.card}>
+                  <img
+                    src={product.picture}
+                    alt={`off coffee for a fake coffee site ${product.id}`}
+                    height={300}
+                    width={200}
+                  />
+                  <h3>{product.name}</h3>
+                  <p>{product.price}</p>
+                  <p>{product.description}</p>
+                  <p>{product.roast}</p>
+                  <button onClick={() => addProductToCart(product)}>
+                    Get Me
+                  </button>
+                  <button onClick={() => removeProductFromCart(product)}>
+                    Ta bort
+                  </button>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </div>
