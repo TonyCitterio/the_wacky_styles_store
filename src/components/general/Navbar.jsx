01d2b91a3415749cad50cdf47d762193c2131b2e
TonyCitterio <tcitterio@hotmail.com>
2023-11-20 11:56:33 +0100

    done with checkout so far

--- src/components/general/Navbar.jsx
+++ src/components/general/Navbar.jsx
@@ -3,11 +3,21 @@ import { FaBasketShopping } from "react-icons/fa6";
 import classes from "./Navbar.module.css";
 import ModalCart from "../cart/ModalCart";
 
-const Navbar = ({ setView, cart, setCart, removeProductFromCart, addProductToCart }) => {
+const Navbar = ({
+  view,
+  setView,
+  cart,
+  setCart,
+  removeProductFromCart,
+  addProductToCart,
+  groupedCart,
+  calculateTotalPrice,
+  calculateTotalCartPrice,
+  handelShippingCostText,
+}) => {
   const [modalOpenCart, setModalOpenCart] = useState(false);
-  const handleViewChange = (newView) => {
-    setView(newView);
-  };
+  const isClickable = cart.length >= 1;
+
   const openCart = () => {
     setModalOpenCart(true);
   };
@@ -16,33 +26,49 @@ const Navbar = ({ setView, cart, setCart, removeProductFromCart, addProductToCar
     <nav className={classes.navbar}>
       <div>
         <h1>The Wacky Coffee Shop</h1>
-        <p>{cart.length}</p>
-        <FaBasketShopping
-          size={30}
-          className={classes.cartIcon}
-          onClick={openCart}
-        />
-        {modalOpenCart && (
-          <ModalCart
-            setModalOpenCart={setModalOpenCart}
-            cart={cart}
-            setCart={setCart}
-            removeProductFromCart={removeProductFromCart}
-            addProductToCart={addProductToCart}
-          />
-        )}
+        {view !== "checkout" ? (
+          <div>
+            <p>{cart.length}</p>
+            <FaBasketShopping
+              size={30}
+              className={classes.cartIcon}
+              onClick={openCart}
+            />
+            {modalOpenCart && (
+              <ModalCart
+                setView={setView}
+                setModalOpenCart={setModalOpenCart}
+                cart={cart}
+                setCart={setCart}
+                removeProductFromCart={removeProductFromCart}
+                addProductToCart={addProductToCart}
+                groupedCart={groupedCart}
+                calculateTotalPrice={calculateTotalPrice}
+                calculateTotalCartPrice={calculateTotalCartPrice}
+                handelShippingCostText={handelShippingCostText}
+              />
+            )}
+            <button
+              onClick={() => setView("checkout")}
+              disabled={!isClickable}
+              style={{ opacity: isClickable ? 1 : 0.5 }}
+            >
+              Till kassan
+            </button>
+          </div>
+        ) : null}
       </div>
-      <div>
-        <ul className={classes.bigUl}>
-          <li onClick={() => handleViewChange("products")}>Hem</li>
-          <li onClick={() => handleViewChange("lightRoast")}>Ljusrost</li>
-          <li onClick={() => handleViewChange("mediumRoast")}>Mellanrost</li>
-          <li onClick={() => handleViewChange("darkRoast")}>Mörkrost</li>
-          <li onClick={() => handleViewChange("extraDarkRoast")}>
-            Extra Mörkrost
-          </li>
-        </ul>
-      </div>
+      {view !== "checkout" ? (
+        <div>
+          <ul className={classes.bigUl}>
+            <li onClick={() => setView("products")}>Hem</li>
+            <li onClick={() => setView("lightRoast")}>Ljusrost</li>
+            <li onClick={() => setView("mediumRoast")}>Mellanrost</li>
+            <li onClick={() => setView("darkRoast")}>Mörkrost</li>
+            <li onClick={() => setView("extraDarkRoast")}>Extra Mörkrost</li>
+          </ul>
+        </div>
+      ) : null}
     </nav>
   );
 };
