4c931de9b53fc2910ad212ae213c8befdb51bbf3
TonyCitterio <tcitterio@hotmail.com>
2023-12-09 17:45:03 +0100

    added the footer

---
+++ src/components/general/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={classes.footer}>
+      <h3>The Wacky Coffee Shop</h3>
+    </footer>
+  );
+};
+
+export default Footer;
